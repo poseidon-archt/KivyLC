2b9a1010e798ca0180d13705b7ca4fa04673d3aa
poseidon-archt <159563769+poseidon-archt@users.noreply.github.com>
2024-02-09 17:52:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,44 @@
-#!/usr/bin/env python
-
-import kivy
-import lovecal
-
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
-from kivy.graphics import Color, Rectangle
-from kivy.core.audio import SoundLoader
-from kivy.uix.textinput import TextInput
-from kivy.core.window import Window
-from kivy.uix.bubble import Bubble
+import os
+import random
+import string
 
-
-class ValidateInput(TextInput):
-    #invalid_set = r"/\*:|'.?" + '"<>'
-
-    invalid_set = r"[-.\'@_!#$%^&*()<>?/\|}{~:0123456789 ]"
-    count = 0
-
-    def insert_text(self, substring, from_undo=False):
-        firstname = [c for c in substring if c not in self.invalid_set]
-        s = ''.join(firstname)
-        if not self.filled:
-            return super().insert_text(s, from_undo=from_undo)
-
-
-#    def keyboard_on_key_up(self, keycode, text):
-#        if self.readonly and text[1] == "backspace":
-#            self.readonly = False
-#            self.do_backspace()
-
-class Manager(ScreenManager):
-
-    result = StringProperty(None)
-    percentage = StringProperty(None)
-
-    def __init__(self, **kwargs):
-        super(Manager, self).__init__(**kwargs)
-
-    def playstart(self):
-        sound = SoundLoader.load('sound/click.wav')
-        sound.play()
-
-    def playreturn(self):
-        sound = SoundLoader.load('sound/click.wav')
-        sound.play()
-
-
-    def results(self):
-        sound = SoundLoader.load('sound/click.wav')
-        print(self.ids.firstname.text)
-        print(self.ids.secondname.text)
-        first = self.ids.firstname.text
-        second = self.ids.secondname.text
-        results = lovecal.calculator(first, second)
-        #prtint(results)
-        if results == "Failed":
-            self.result = "Failed"
-            self.percentage = "Failed"
-        else:
-            self.result = results['result']
-            self.percentage = results['percentage']
-        #self.result = results[1]
-        #self.percentage = results[0]
-        print(self.result)
-        print(self.percentage)
-        #print(str(results['percentage']))
-        #print(str(results['result']))
-        sound.play()
-
-class ScreenApp(App):
+class MyApp(App):
     def build(self):
-        self.icon = 'images/appicon.png'
-        self.sm = Manager()
-        return self.sm
+        self.layout = BoxLayout(orientation='vertical')
 
-Window.softinput_mode = "below_target"
+        # Label pour afficher le répertoiyre courhant
+        self.current_dir_label = Label(
+            text=f"Répertoire courant : {os.getcwd()}"
+        )
+        self.layout.add_widget(self.current_dir_label)
+
+        # Bouton pourj créer un fichier texte avec un nom aléatoire
+        self.create_file_button = Button(
+            text="Créerr un fichier",
+            pos_hint={'center_x': 0.5},
+            on_press=self.create_random_file
+        )
+        self.layout.add_widget(self.create_file_button)
+
+        return self.layout
+
+    def create_random_file(self, instance):
+        # Générer un nom de fichier aléatoire
+        random_filename = ''.join(random.choices(string.ascii_letters, k=8)) + ".txt"
+
+        # Chemin complet du fichier dans le répertoire courant
+        file_path = os.path.join(os.getcwd(), random_filename)
+
+        # Créer le fichier texte
+        with open(file_path, 'w') as file:
+            file.write("Contenu du fichier.")
+
+        # Mettre à jour le label avec le nouveau répertoire courant
+        self.current_dir_label.text = f"Répertoire courant : {os.getcwd()}"
 
 if __name__ == '__main__':
-    ScreenApp().run()
+    MyApp().run()
