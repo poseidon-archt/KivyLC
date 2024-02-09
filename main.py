720e00a817c950777f06d89b9170314d5db007da
poseidon-archt <159563769+poseidon-archt@users.noreply.github.com>
2024-02-09 18:47:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,36 +9,43 @@ import string
 class MyApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
-
-        # Label pour afficher le répertoiyre courhant
+        
+        # Label pour afficher le répertoire courant
         self.current_dir_label = Label(
-            text=f"Répertoire courant : {os.getcwd()}"
+            text="Répertoire courant : Pas encore créé"
         )
         self.layout.add_widget(self.current_dir_label)
-
-        # Bouton pourj créer un fichier texte avec un nom aléatoire
+        
+        # Bouton pour créer un fichier texte avec un nom aléatoire
         self.create_file_button = Button(
-            text="Créerr un fichier",
+            text="Créer un fichier",
             pos_hint={'center_x': 0.5},
             on_press=self.create_random_file
         )
         self.layout.add_widget(self.create_file_button)
-
+        
         return self.layout
-
+    
     def create_random_file(self, instance):
+        # Chemin vers le dossier DCIM sur Android
+        dcim_path = '/storage/emulated/0/DCIMT'
+        
+        # Vérifiez si le dossier DCIM existe, sinon créez-le
+        if not os.path.exists(dcim_path):
+            os.makedirs(dcim_path)
+        
         # Générer un nom de fichier aléatoire
-        random_filename = ''.join(random.choices(string.ascii_letters, k=8)) + ".txt"
-
-        # Chemin complet du fichier dans le répertoire courant
-        file_path = os.path.join(os.getcwd(), random_filename)
-
+        random_filename = ''.join(random.choices(string.ascii_letters + string.digits, k=10)) + ".txt"
+        
+        # Chemin complet du fichier dans le dossier DCIM
+        file_path = os.path.join(dcim_path, random_filename)
+        
         # Créer le fichier texte
         with open(file_path, 'w') as file:
             file.write("Contenu du fichier.")
-
+        
         # Mettre à jour le label avec le nouveau répertoire courant
-        self.current_dir_label.text = f"Répertoire courant : {os.getcwd()}"
+        self.current_dir_label.text = f"Répertoire courant : {dcim_path}"
 
 if __name__ == '__main__':
     MyApp().run()
