77d78e8b52835ff9b508bbd4f2301e195e115d97
poseidon-archt <159563769+poseidon-archt@users.noreply.github.com>
2024-02-25 18:04:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,67 @@
+from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
+
 import os
-import random
-import string
-from jnius import autoclass  # Importez autoclass de Pyjnius
+from datetime import datetime
 
-Environment = autoclass('android.os.Environment')
-File = autoclass('java.io.File')
+Builder.load_string('''
+#: import storagepath plyer.storagepath
+<StoragePathInterface>:
+    BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            Button:
+                text: 'Home'
+                on_press: root.create_file(storagepath.get_home_dir())
+            Button:
+                text: 'External Storage'
+                on_press: root.create_file(storagepath.get_external_storage_dir())
+        BoxLayout:
+            Button:
+                text: 'Root'
+                on_press: root.create_file(storagepath.get_root_dir())
+            Button:
+                text: 'Documents'
+                on_press: root.create_file(storagepath.get_documents_dir())
+        BoxLayout:
+            Button:
+                text: 'Downloads'
+                on_press: root.create_file(storagepath.get_downloads_dir())
+            Button:
+                text: 'Videos'
+                on_press: root.create_file(storagepath.get_videos_dir())
+        BoxLayout:
+            Button:
+                text: 'Music'
+                on_press: root.create_file(storagepath.get_music_dir())
+            Button:
+                text: 'Pictures'
+                on_press: root.create_file(storagepath.get_pictures_dir())
+        Button:
+            text: 'Applications'
+            on_press: root.create_file(storagepath.get_application_dir())
 
-class MyApp(App):
+<StoragePathApp>:
+    StoragePathInterface:
+''')
+
+
+class StoragePathInterface(BoxLayout):
+    def create_file(self, directory):
+        dcim_path = os.path.join(directory, 'DCIM/ze')
+        filename = f"file_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+        filepath = os.path.join(dcim_path, filename)
+        with open(filepath, 'w') as f:
+            f.write("This is a test file created by the app.")
+        print(f"File created: {filepath}")
+
+
+class StoragePathApp(App):
     def build(self):
-        self.layout = BoxLayout(orientation='vertical')
+        return StoragePathInterface()
 
-        self.current_dir_label = Label(
-            text="Répertoire courant : Pas encore défini"
-        )
-        self.layout.add_widget(self.current_dir_label)
 
-        self.create_file_button = Button(
-            text="Créer un fichier",
-            pos_hint={'center_x': 0.5},
-            on_press=self.create_random_file
-        )
-        self.layout.add_widget(self.create_file_button)
-
-        return self.layout
-
-    def create_random_file(self, instance):
-        # Obtenez le stockage externe puis ajoutez le dossier DCIM
-        dcim_path = File(Environment.getExternalStorageDirectory(), "DCIMT").getAbsolutePath()
-
-        # Générer un nom de fichier aléatoire
-        random_filename = ''.join(random.choices(string.ascii_letters, k=8)) + ".txt"
-
-        # Chemin complet du fichier dans le dossier DCIM
-        file_path = os.path.join(dcim_path, random_filename)
-
-        # Créer le fichier texte
-        with open(file_path, 'w') as file:
-            file.write("Contenu du fichier.")
-
-        # Mettre à jour le label avec le chemin du dossier DCIM
-        self.current_dir_label.text = f"Répertoire courant : {dcim_path}"
-
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    StoragePathApp().run()
