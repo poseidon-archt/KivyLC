9e37cd3b5e1dff28bed90e32639af6b1bfbf20fd
poseidon-archt <159563769+poseidon-archt@users.noreply.github.com>
2024-03-06 17:48:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,12 @@
-from kivy.lang import Builder
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.label import Label
 import os
-from datetime import datetime
-import shutil
 
-Builder.load_string('''
-<StoragePathInterface>:
-    BoxLayout:
-        orientation: 'vertical'
-        BoxLayout:
-            Button:
-                text: 'External Storage'
-                on_press: root.create_file(app.external_storage_path)
-            Button:
-                text: 'Documents'
-                on_press: root.create_file(app.documents_path)
-        BoxLayout:
-            Button:
-                text: 'Downloads'
-                on_press: root.create_file(app.downloads_path)
-            Button:
-                text: 'Pictures'
-                on_press: root.create_file(app.pictures_path)
-
-<StoragePathApp>:
-    StoragePathInterface:
-''')
-
-class StoragePathInterface(BoxLayout):
-    def create_file(self, directory):
-        if not os.path.exists(directory):
-            try:
-                os.makedirs(directory)
-            except PermissionError:
-                # Fallback sur le répertoire de l'application si les permissions sont insuffisantes
-                directory = App.get_running_app().user_data_dir
-
-        filename = f"file_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-        filepath = os.path.join(directory, filename)
-
-        with open(filepath, 'w') as f:
-            f.write("This is a test file created by the app.")
-
-        print(f"File created: {filepath}")
-
-class StoragePathApp(App):
-    external_storage_path = os.path.join(os.environ.get('EXTERNAL_STORAGE'), 'DCIM/ze')
-    documents_path = os.path.join(os.environ.get('EXTERNAL_STORAGE'), 'Documents')
-    downloads_path = os.path.join(os.environ.get('EXTERNAL_STORAGE'), 'Downloads')
-    pictures_path = os.path.join(os.environ.get('EXTERNAL_STORAGE'), 'Pictures')
-
+class MyApp(App):
     def build(self):
-        return StoragePathInterface()
+        current_dir = os.getcwd()
+        label = Label(text=current_dir)
+        return label
 
-if __name__ == "__main__":
-    StoragePathApp().run()
+if __name__ == '__main__':
+    MyApp().run()
